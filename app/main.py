dc6030eb2c5689b70cbe0f7c98b5553e012f0308
AbhiramKrishnaM <abhiramKrishna.8921@gmail.com>
2026-03-11 23:59:56 +0530

    trying out few basics

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
-from enum import Enum
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str
+    price: int
 
 # check if the server is running 
 @app.get("/")
 async def root():
     return {"message":"server is up and running!"}
+
+
+@app.get("/return-items")
+def return_items():
+    return Item(name="soap", description="soap is a good item", price=20)
