f97c2c25261f5160044398559db947b28561c4c8
AbhiramKrishnaM <abhiramKrishna.8921@gmail.com>
2026-03-15 23:43:55 +0530

    intercept endpoint

--- app/main.py
+++ app/main.py
@@ -3,17 +3,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    description: str
-    price: int
+
+class InterceptRequest(BaseModel):
+    action: str  =  'delete' | 'read' | 'update' | 'create'
+class InterceptResponse(BaseModel):
+    status: str
+    risk_score: float
 
 # check if the server is running 
 @app.get("/")
 async def root():
     return {"message":"server is up and running!"}
 
-# post
-@app.post("/items")
-def create_item(item:Item):
-    return item 
+# intercept endpoint
+@app.post("/intercept")
+def intercept(request: InterceptRequest)-> InterceptResponse:
+    return InterceptResponse(status="allowed", risk_score=0.3)
