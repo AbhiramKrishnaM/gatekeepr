5de9c5f789f879255d57dda30776bec5bf94724c
AbhiramKrishnaM <abhiramKrishna.8921@gmail.com>
2026-03-15 23:51:45 +0530

    Types defined

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 from pydantic import BaseModel
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
-
+class ActionType(Enum):
+    DELETE = "delete"
+    READ = "read"
+    UPDATE = "update"
+    CREATE = "create"
+ 
 class InterceptRequest(BaseModel):
-    action: str  =  'delete' | 'read' | 'update' | 'create'
+    action: ActionType
+    agent_id: str
+    target_resource: str 
+    target_id: str
+    action_details: dict
+    context: dict
 class InterceptResponse(BaseModel):
     status: str
     risk_score: float
